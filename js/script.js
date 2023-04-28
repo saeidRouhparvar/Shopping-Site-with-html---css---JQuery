deff91d074c269920c42448e328ad019f3bfd611
saeid <saeid.rohparvar@gmail.com>
2023-04-28 19:19:25 +0330

    navbar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).on('click', '.search', function () {
+    $('.search-bar').addClass('search-bar-active')
+});
+$(document).on('click', '.search-cancel', function () {
+    $('.search-bar').removeClass('search-bar-active')
+});
