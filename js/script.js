0bf2a353adef4fe9198dc4de00f1b6a30445fe67
saeid <saeid.rohparvar@gmail.com>
2023-04-28 22:16:19 +0330

    login form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+// search-bar
 $(document).on('click', '.search', function () {
     $('.search-bar').addClass('search-bar-active')
 });
 $(document).on('click', '.search-cancel', function () {
     $('.search-bar').removeClass('search-bar-active')
+});
+
+// Login-form 
+$(document).on('click','.user,.already-account', function(){
+    $('.form').addClass('login-active').removeClass('sign-up-active')
+});
+$(document).on('click','.sign-up-btn', function(){
+    $('.form').addClass('sign-up-active').removeClass('login-active')
+});
+$(document).on('click','.form-cancel', function(){
+    $('.form').removeClass('login-active').removeClass('sign-up-active')
 });
