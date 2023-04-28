86a827bb7a3d7c34f8b6c4bc827a2fda821a4e08
saeid <saeid.rohparvar@gmail.com>
2023-04-28 23:26:26 +0330

    slider

--- js/script.js
+++ js/script.js
@@ -7,12 +7,24 @@ $(document).on('click', '.search-cancel', function () {
 });
 
 // Login-form 
-$(document).on('click','.user,.already-account', function(){
+$(document).on('click', '.user,.already-account', function () {
     $('.form').addClass('login-active').removeClass('sign-up-active')
 });
-$(document).on('click','.sign-up-btn', function(){
+$(document).on('click', '.sign-up-btn', function () {
     $('.form').addClass('sign-up-active').removeClass('login-active')
 });
-$(document).on('click','.form-cancel', function(){
+$(document).on('click', '.form-cancel', function () {
     $('.form').removeClass('login-active').removeClass('sign-up-active')
+});
+
+// Full Slider 
+
+$(document).ready(function () {
+    $('#adaptive').lightSlider({
+        adaptiveHeight: true,
+        auto:true,
+        item: 1,
+        slideMargin: 0,
+        loop: true
+    });
 });
